f83c4831bd77a2171d7831f3ef628c0cf5a7c19e
natansilva13 <natan.silva13@escola.pr.gov.br>
2026-03-26 08:48:51 -0300

    Create cont18.py

---
+++ Conteúdos/cont18.py
@@ -0,0 +1,5 @@
+from math import sqrt, floor, ceil
+num = int(input('Digite um número: ' ))
+raiz = math.sqrt(num)
+print('a raiz de {} é igual a {}'.format(num, ceil(raiz)))
+print('a raiz de {} é igual a {}'.format(num, floor(raiz)))
