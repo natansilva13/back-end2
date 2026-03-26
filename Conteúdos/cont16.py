b6e45d0b51235bc64259ea24699bde3375d44065
natansilva13 <natan.silva13@escola.pr.gov.br>
2026-03-26 08:27:35 -0300

    Delete Conteúdos/cont16.py

--- Conteúdos/cont16.py
+++
@@ -1,2 +0,0 @@
-Para importar bibliotecas e funcionalidades em Python utiliza-se o comando Import seguido do nome da biblioteca ou módulo que ira ser carregado nesse programa.
-Para importar coisa específicas de uma bibllioteca utiliza-se o comando from seguido do nome da biblioteca seguido da palavra import seguido do item que deseja importar.
